df23e80090ebf0357437a9b94b6999b639641235
Krunal Pawar <krunal.p@digitalcubez.com>
2022-05-20 15:24:43 +0530

    fix name

--- database/migrations/2022_04_05_190760_add_read_only_column_to_quizzes_table.php
+++ database/migrations/2022_04_05_190760_add_read_only_column_to_quizzes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsReadOnlyFieldToQuizzes extends Migration
+class AddReadOnlyColumnToQuizzes extends Migration
 {
     /**
      * Run the migrations.
